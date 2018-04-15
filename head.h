78016b540f5e3fa65cc42cc9c11d33be06bad40f
liyusky <452082031@qq.com>
2018-04-15 16:47:30 +0800

    shell code finished

---
+++ head.h
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <sys/time.h>
+#include <dirent.h>
+
+//用户命令字符串缓冲区大小
+int g_commandMax = 1024;
+//用户命令分割后的最大支持数量
+int g_commandMaxWords = 128;
+//路径缓冲区大小
+int g_currentDirectoryMax = 1024;
+//输出描述符
+int g_out;
+//输入描述符
+int g_in;
+
+int running_pid=0;
+
+//PID链表
+struct process{
+    int id;
+    struct process *next;
+}*head=NULL;
+struct process* addProcessInList(int process_id, struct process *head){
+
+    struct process *newNode = (struct process*)malloc(sizeof(struct process));
+
+    if(newNode == NULL) {
+        fprintf(stderr, "Unable to allocate memory for new node\n");
+        exit(-1);
+    }
+
+    newNode->id = process_id;
+    newNode->next = NULL;
+
+    if(head == NULL){
+        head = newNode;
+        return head;
+    } else {
+        struct process *current = head;
+        while (current->next!=NULL) {
+            current=current->next;
+        };
+        current->next=newNode;
+        return head;
+    }
+
+}
+struct process* deleteFirstProcess(struct process *head){
+
+    struct process *temp;
+    temp=head->next;
+    free(head);
+    return temp;
+
+}
+
+
+int endsWith(char *s,char *end);
+char* getHomeDir();
+int changeDir(char* to);
+void Deallocate(char **array,int length);
+int Launch(char** args,int background);
+int ExecuteInput(char** args, int length,int background);
+char* ReadInput();
+char** TokenizeInput(char* input,int* length);
+int Redirect(char* redirectSymbol,char* filename);
+char** ParseInput(char* input, int* length,int* background);
+void Round_Robbin(int signal);
---
+++ main.c
@@ -0,0 +1,52 @@
+#include "head.h"
+#include "func.h"
+
+int main()
+{
+	//用户输入的字符串在这里
+    char* input;
+	//切分后的字符串数组在这里
+    char** args;
+    int length;
+
+    // 把当前shell的工作路径设置为当前用户的home路径
+    char* homeDir = getHomeDir();
+    changeDir(homeDir);
+	
+    int background = 0;
+
+	
+    struct timeval value={1,0};
+	struct timeval interval={1,0};
+	struct itimerval timer={interval,value};
+	setitimer(ITIMER_REAL, &timer, 0);
+
+	//轮询，一秒执行一次Round_Robbin函数
+	signal(SIGALRM,&Round_Robbin);
+
+	//主循环
+    while (1) {
+        
+        errno = 0;
+        char cwd[g_currentDirectoryMax];
+		
+		//当前工作路径
+        getcwd(cwd, sizeof(cwd)); 
+        if (errno) {
+            perror("An error occured");
+            exit(EXIT_FAILURE);
+        }
+		
+		//每行命令的prefix和路径提示
+        printf("MyShell:%s:$ ", cwd);
+		
+		//读输入，执行
+        input = ReadInput(); 
+        args = ParseInput(input, &length, &background);
+        if(ExecuteInput(args,length,background) == EXIT_FAILURE) {
+                return EXIT_FAILURE;
+        }
+    }
+
+    return 0;
+}
